bb969b74822d27fbb131ca5ccfba359a884edccb
00009054 <00009054.wiut@gmail.com>
2020-12-08 20:27:04 +0500

    Create DOM elemnt on JS

---
+++ js/add-review.js
@@ -0,0 +1,37 @@
+function addReview () {
+  
+  const newLi = document.createElement('LI')
+  newLi.textContent = review.value
+  
+  reviewList.appendChild(newLi)
+  
+  review.value = '' 
+  
+  const newBtn = document.createElement('BUTTON')
+  newLi.appendChild(newBtn)
+  
+  newBtn.onclick = function () {
+    newLi.remove()
+    newBtn.remove()
+  }
+}
+
+plusButton.onclick = addReview
+
+review.onkeyup = function (event) {
+  if (event.keyCode === 13) {
+    
+    const newLi = document.createElement('LI')
+    newLi.textContent = review.value
+    reviewList.appendChild(newLi)
+    review.value = '' 
+    
+    const newBtn = document.createElement('BUTTON')
+    newLi.appendChild(newBtn)
+    
+    newBtn.onclick = function () {
+      newLi.remove()
+      newBtn.remove()
+    }
+  }
+}
